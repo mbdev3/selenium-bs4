081a79948e1d98f9af3dc2868dadc0d7850b6041
Mohammed Banani | MbDev <leedbn1@gmail.com>
2022-09-19 22:54:08 +0100

    ok

--- Shein scraping/seleniumshein.py
+++ Shein scraping/seleniumshein.py
@@ -12,9 +12,9 @@ options = Options()
 
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.BinaryLocation = "C:/Users/moham_nbdosir/AppData/Local/Google/Chrome SxS/Application/chrome.exe"
-options.add_argument('headless')
+# options.add_argument('headless')
 
-url = 'https://eur.shein.com/Women-T-Shirts-c-1738.html?ici=eur_tab01navbar04menu01dir01&scici=navbar_WomenHomePage~~tab01navbar04menu01dir01~~4_1_1~~real_1738~~~~0&src_module=topcat&src_tab_page_id=page_home1656085819680&src_identifier=fc%3DWomen%60sc%3DCLOTHING%60tc%3DTOPS%60oc%3DT-Shirts%60ps%3Dtab01navbar04menu01dir01%60jc%3Dreal_1738&srctype=category&userpath=category-CLOTHING-TOPS-T-Shirts'
+url = 'https://eur.shein.com/Women-Tops,-Blouses-Tee-c-1766.html?ici=eur_tab01navbar04menu01&scici=navbar_WomenHomePage~~tab01navbar04menu01~~4_1~~real_1766~~~~0&src_module=topcat&src_tab_page_id=page_real_class1663437350149&src_identifier=fc%3DWomen%60sc%3DCLOTHING%60tc%3DTOPS%60oc%3D0%60ps%3Dtab01navbar04menu01%60jc%3Dreal_1766&srctype=category&userpath=category-CLOTHING-TOPS'
 browser_driver = Service(executable_path='C:/Users/moham_nbdosir/AppData/Local/Google/Chrome SxS/chromedriver.exe')
 page_to_scrape = webdriver.Chrome(service=browser_driver,options=options)
 
@@ -23,7 +23,7 @@ page_to_scrape.get(url=url)
 file  = open("shein.csv","w")
 writer = csv.writer(file)
 
-writer.writerow(["NAME",'IMG','PRICE'])
+writer.writerow(["NAME",'IMG','PRICE','COLORS'])
 
 
 closex = page_to_scrape.find_element(By.XPATH, "/html/body/div[1]/div[5]/div[1]/div/i")
@@ -35,12 +35,14 @@ html.send_keys(Keys.END)
 names = page_to_scrape.find_elements(By.CLASS_NAME,'S-product-item__link')
 imgs = page_to_scrape.find_elements(By.CLASS_NAME,'S-product-item__img-submain')
 prices = page_to_scrape.find_elements(By.CLASS_NAME,'S-product-item__retail-price')
-
+colors = page_to_scrape.find_elements(By.CLASS_NAME,'S-product-item__relatecolor-inner falcon-lazyload')
 # imgs = page_to_scrape.find_element_by_xpath("//img[contains(@class,'S-product-item__img-submain')]")
 # for name, img in zip(names,imgs):
 # 	print(name.text + "-" + img.text)
 # 	writer.writerow([img.text,img.text])
-for name,img,price in zip(names,imgs,prices):
-	print(price.text)
-	writer.writerow([name.text,img.get_attribute('data-src'),price.text])
+for name,img,price,color in zip(names,imgs,prices,colors):
+	for singleColor in color:
+		print(singleColor)
+		writer.writerow([singleColor.get_attribute('data-background-image')])
+	writer.writerow([name.text,img.get_attribute('data-src'),price.text,color.get_attribute('data-background-image')])
 file.close()
